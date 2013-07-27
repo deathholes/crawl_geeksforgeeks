be117abd7262437ad77491982ba671bdaff68cd0
Subhendu Malakar <malakar.subhendu@gmail.com>
2013-07-27 05:51:52 +0000

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,14 +5,15 @@ import string
 import queue
 
 def add_links(st,q):
-  dat=st.split('\n')
+	dat=st.split('\n')
 	black=('feed','.','#')
 	for s in dat:
 		if 'http://www.geeksforgeeks.org/' in s:
 			link=s.partition('href="http://www.geeksforgeeks.org/')
 			l,p,link=link[2].partition('"')
-			if 'feed' not in l and '.' not in l and '#' not in l and l != None:
-				q.put('http://www.geeksforgeeks.org/'+l)
+			if l:
+				if 'feed' not in l and '.' not in l and '#' not in l and 'forums' not in l and 'contribute' not in l:
+					q.put('http://www.geeksforgeeks.org/'+l)
 			
 
 def format_string(data,name):
@@ -39,21 +40,30 @@ def main():
 		if url in lst:
 			continue
 		lst.append(url)
-		request = urllib.request.Request(url)
-		request.add_header("User-Agent", "My Crawler")
-		opener = urllib.request.build_opener()
-		f = opener.open(request)
-		#f = urllib.request.urlopen(url)
-		st = f.read().decode('utf-8');
-		add_links(st,q)
-		filename = url.partition('org/')
-		filename,fil,fi = filename[2].partition('/')
+		try:
+			request = urllib.request.Request(url)
+			request.add_header("User-Agent", "My Crawler")
+			opener = urllib.request.build_opener()
+			f = opener.open(request)
+			#f = urllib.request.urlopen(url)
+			st = f.read().decode('utf-8');
+		except IOError:
+			continue
+		except UnicodeError:
+			continue
+			
+		fil,f,filename = url.partition('org/')
+		if filename[-1:] == '/':
+			filename=filename[:-1]
+		while '/' in filename:
+			fil,f,filename = filename.partition('/')
 		write_this = '<html> \n <head> ' + filename + ' \n <title>' +  filename +' </title>\n</head>\n <body>\n'
 		st=write_this + format_string(st,url) + '</body>';
 		fobj=open(filename,'w')
 		fobj.write(st)
 		fobj.close()
 		print(filename+'\n')
+		add_links(st,q)
 	return 0
 
 if __name__ == '__main__':
